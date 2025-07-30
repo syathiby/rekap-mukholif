13a429b0d05faebe3c644ffebc812247396f3ece
AuthorBe <authorbe45@gmail.com>
2025-07-30 16:20:10 +0700

    Update

--- rekap/kamar-terkotor.php
+++ rekap/kamar-terkotor.php
@@ -30,6 +30,7 @@ $result = mysqli_query($conn, $query) or die("Query Error: " . mysqli_error($con
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/table-responsive.css">
     <style>
         :root {
             --primary: #4361ee;
