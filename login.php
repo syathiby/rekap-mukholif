7aee2ef648e64b9780f424a747ab2b9eaac9e028
AuthorBe <authorbe45@gmail.com>
2025-10-07 17:36:55 +0700

    logo login

--- login.php
+++ login.php
@@ -57,6 +57,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Sistem</title>
+    <link rel="icon" type="image/png" sizes="64x64" href="/assets/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
