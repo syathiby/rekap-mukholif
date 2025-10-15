668280133c839bfefc8564d077876fbdf2549054
AuthorBe <authorbe45@gmail.com>
2025-10-15 17:09:10 +0700

    rekap umum new

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistem Pelanggaran Santri</title>
+    <title>Aplikasi Pendataan Mukholif</title>
     
     <!-- Aset & Font -->
     <!-- REVISI: Path favicon jadi dinamis pake BASE_URL -->
