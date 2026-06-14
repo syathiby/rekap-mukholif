179fb5718874c6b22651e9ec5200b13d4f33223b
AuthorBe <authorbe45@gmail.com>
2026-06-14 20:19:23 +0700

    fix : perbaikan struktur file bagian rapot dan arsip.

--- arsip/create.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Fitur pembuatan arsip manual telah dinonaktifkan.
-// Arsip kini dibuat secara otomatis melalui fitur Tutup Buku Akhir Tahun (Pengaturan -> Tutup Buku).
-header("Location: index.php");
-exit();
-?>
